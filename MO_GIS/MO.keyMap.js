07a9e7f85f0a4153ca5517af45f363f6dcd267d5
JHOH <jhoh@mind-one.co.kr>
2023-10-12 19:00:07 +0900

    전역 KEY-Alias 매칭 트릭

---
+++ MO_GIS/MO.keyMap.js
@@ -0,0 +1,36 @@
+
+/*
+ * 쿼리에서 사용한 alias 와, 
+ * 소스 상 사용하는 객체들의 KEY 값을 
+ * 일치시키기 위한 변수정의
+ */
+
+/* △△△△△△레이어 식별 관련△△△△△△△△△△△△△ */
+export const LAYER_NAME = `layerTitle` //화면에서 표현될 레이어 제목
+export const TYPE_NAME = `typeName`; //geoserver 용 레이어 식별자
+export const LAYER_TYPE = `layerType`; //BASE, group, POINT, LINE, POLYGON 등 레이어 타입
+
+/* △△△△△△레이어 스타일(Style) 관련△△△△△△△△△△△△△ */
+export const LINE_WIDTH = `lineWidth`;
+export const COLOR_FILL = `colorFill`;
+export const COLOR_LINE = `colorLine`;
+export const ICON_NAME = `iconName`;
+export const MIN_ZOOM = `minZoom`;
+export const Z_INDEX = `zIndex`;
+export const BOOL_VISIBLE = `boolIsdefault`;
+export const BOOL_IS_LAYER = `boolIsgroup`;
+
+/* ⭕레이어 트리(Tree)용 JSON 관련 🚫 */
+export const ELEMENT_ID = `id`;
+export const PARENT_ID = `pid`;
+export const CHILD_MARK = `childList`;
+
+/* ▲▲▲▲▲레이어 소스(Source) 관련▲▲▲▲▲▲▲▲▲△ */
+export const ORIGIN = `origin`;
+export const SOURCE_PATHNAME = `sourcePathname`;
+export const SOURCE_TYPE = `sourceType`;
+export const CQL_FILTER = `cqlfilter`;
+export const APIKEY = `apiKey`;
+export const SRID = `srid`;
+export const CATEGORY = `category`;
+    
