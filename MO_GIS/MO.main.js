f5fd6ee46c49105db6719fa2aeaa92f4b211bb9b
JHOH <jhoh@mind-one.co.kr>
2023-10-20 15:13:29 +0900

    vworld 사용예시 + Spinner 사용

--- MO_GIS/MO.main.js
+++ MO_GIS/MO.main.js
@@ -100,24 +100,14 @@ map.addLayer(layer);
 //----SPINNER
 
  let op = {
-     lines: 15,
-     length: 38,
-     width: 12,
-     radius: 38,
-     scale: 1,
-     corners: 1,
-     speed: 1,
-     rotate: 0,
+     lines: 15, length: 38, width: 12, radius: 38,
+     scale: 1, corners: 1, speed: 1, rotate: 0,
      animation: "spinner-line-fade-more",
-     direction: "1",
-     color: "#ffffff",
+     direction: "1", color: "#ffffff",
      fadeColor: "transparent",
-     top: "50%",
-     left: "50%",
-     shadow: "grey 3px 4px 8px 1px",
+     top: "50%", left: "50%", shadow: "grey 3px 4px 8px 1px",
      zIndex: 2000000000,
-     className: "spinner",
-     position: "absolute",
+     className: "spinner", position: "absolute",
  };
 
 let spin = new Spinner(op).spin(document.querySelector('#map'))
