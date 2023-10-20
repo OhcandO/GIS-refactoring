fd8c5a5a596d5bc894829ca838810ad02faef7e3
JHOH <jhoh@mind-one.co.kr>
2023-10-20 17:00:54 +0900

    스타일 팩토리 수정

--- MO_GIS/classes/MO.StyleFactory.js
+++ MO_GIS/classes/MO.StyleFactory.js
@@ -1,11 +1,13 @@
-import Fill from '../../lib/openlayers_v7.5.1/style/Fill.js';
-import Icon from '../../lib/openlayers_v7.5.1/style/Icon.js';
-import Style,{StyleFunction} from '../../lib/openlayers_v7.5.1/style/Style.js';
 import * as KEY from '../common/MO.keyMap.js';
 import { MOFactory } from './abstract/MO.Factory';
-import CircleStyle from '../../lib/openlayers_v7.5.1/style/Circle.js'
+import Style,{StyleFunction} from '../../lib/openlayers_v7.5.1/style/Style.js';
 import Stroke from '../../lib/openlayers_v7.5.1/style/Stroke.js';
+import Fill from '../../lib/openlayers_v7.5.1/style/Fill.js';
 import Text from '../../lib/openlayers_v7.5.1/style/Text.js';
+import Icon from '../../lib/openlayers_v7.5.1/style/Icon.js';
+import CircleStyle from '../../lib/openlayers_v7.5.1/style/Circle.js'
+import { MultiPolygon, Polygon } from '../../lib/openlayers_v7.5.1/geom.js';
+import Feature from '../../lib/openlayers_v7.5.1/Feature.js';
 /**
  * 이 JS 파일에서 아이콘 경로까지의 relative path
  */
@@ -21,7 +23,7 @@ const iconPath= `../images/icons/`;
 export class StyleFactory extends MOFactory {
     #default_icon = {
         crossOrigin: "anonymous",
-        // src: undefined,
+        src: undefined,
         anchor: [0.5, 0.5], //이미지 정 중앙
         displacement: [0, 0], //이격정도 [5, 6] : 오른쪽으로 5, 위쪽으로 6
         opacity: 1, //투명도 0~1
@@ -59,8 +61,22 @@ export class StyleFactory extends MOFactory {
 
 
 
+    /**
+     * 적합한 레이어 타입을 판별하고 대문자로 반환
+     * @returns {String}
+     */
     #getLayerType() {
-        return this.layerCode[KEY.LAYER_TYPE];
+        const validLayerTypes = ['POINT','LINE','POLYGON' /* ,'group','BASE' */]; //'group', 'BASE' 는 DB상 나눠놓은 코드이고, 벡터 데이터는 아니므로 여기서는 제외
+        let layerType = this.layerCode[KEY.LAYER_TYPE];
+        if(layerType){
+            layerType = layerType.toUpperCase();
+            if(validLayerTypes.some(type=>type.toUpperCase()===layerType)){
+                return layerType;
+            }
+        }else{
+            console.log(`valid layerTypes : `, validLayerTypes);
+            throw new Error(`layerType이 적합하지 않음 : ${layerType}`)
+        }
     }
 
     /**
@@ -73,12 +89,15 @@ export class StyleFactory extends MOFactory {
         this.updateLayerCode();
         let styleFunc;
 
-        let type = this.#getLayerType();
-        if (type === KEY.LAYER_TYPE_POINT) {
+        let type;
+        try{
+            type = this.#getLayerType();
+        }catch(e){console.error(e)}
+        if (type === KEY.OL_FEATURE_TYPE_POINT) {
             styleFunc = this.#getStyleFunc_POINT();
-        } else if (type === KEY.LAYER_TYPE_LINE) {
+        } else if (type === KEY.OL_FEATURE_TYPE_LINE) {
             styleFunc = this.#getStyleFunc_LINE();
-        } else if (type === KEY.LAYER_TYPE_POLYGON) {
+        } else if (type === KEY.OL_FEATURE_TYPE_POLYGON) {
             styleFunc = this.#getStyleFunc_POLYGON();
         }
 
@@ -93,23 +112,31 @@ export class StyleFactory extends MOFactory {
      * @memberof StyleFactory
      */
     updateLayerCode() {
-        this.#default_text.font =
-            this.layerCode[KEY.FONT_STYLE] ?? this.#default_text;
+        if (this.layerCode[KEY.ICON_NAME]) this.#default_icon.src = iconPath + this.layerCode[KEY.ICON_NAME];
+        else delete this.#default_icon.src; 
+        
+        this.#default_text.font = this.layerCode[KEY.FONT_STYLE] ?? this.#default_text;
+        
+        this.#default_text_stroke.color = this.layerCode[KEY.FONT_OUTLINE] ?? this.#default_text_stroke.color;
+        this.#default_text_fill.color = this.layerCode[KEY.FONT_FILL] ?? this.#default_text_fill.color;
 
-        this.#default_text_stroke.color =
-            this.layerCode[KEY.FONT_OUTLINE] ?? this.#default_text_stroke.color;
-        this.#default_text_fill.color =
-            this.layerCode[KEY.FONT_FILL] ?? this.#default_text_fill.color;
+        this.#default_stroke.color = this.layerCode[KEY.COLOR_LINE] ?? this.#default_stroke.color;
+        this.#default_stroke.width = this.layerCode[KEY.LINE_WIDTH] ?? this.#default_stroke.width;
+        this.#default_stroke.lineDash = this.layerCode[KEY.LINE_STYLE] ?? this.#default_stroke.lineDash;
 
-        this.#default_stroke.color =
-            this.layerCode[KEY.COLOR_LINE] ?? this.#default_stroke.color;
-        this.#default_stroke.width =
-            this.layerCode[KEY.LINE_WIDTH] ?? this.#default_stroke.width;
-        this.#default_stroke.lineDash =
-            this.layerCode[KEY.LINE_STYLE] ?? this.#default_stroke.lineDash;
+        this.#deafult_fill.color = this.layerCode[KEY.COLOR_FILL] ?? this.#deafult_fill.color;
 
-        this.#deafult_fill.color =
-            this.layerCode[KEY.COLOR_FILL] ?? this.#deafult_fill.color;
+        //레이어 구성하는 피쳐 타입에 따라 특수한 스타일 오버라이딩
+        let layerType;
+        try{
+            layerType = this.#getLayerType();
+        }catch(e){console.error(e)}
+
+        if(layerType == KEY.OL_FEATURE_TYPE_LINE){
+            this.#default_text.placement='line';
+        }else if (layerType == KEY.OL_FEATURE_TYPE_POLYGON){
+            this.#default_icon.scale = 1.5;
+        }
     }
 
     /**
@@ -123,26 +150,13 @@ export class StyleFactory extends MOFactory {
             //1. 포인트 객체에 아이콘 이름 할당되엇으
             let icon;
             if (me.layerCode[KEY.ICON_NAME]) {
-                let iconOption = me.filterNullishVals(
-                    Object.assign({}, me.#default_icon, {
-                        src: iconPath + me.layerCode[KEY.ICON_NAME],
-                    })
-                );
-                try {
-                    icon = new Icon(iconOption);
-                } catch (e) {
-                    console.error(e);
-                }
+                icon = new Icon(me.#default_icon);
             } else {
-                try {
-                    icon = new CircleStyle({
-                        fill: new Fill(me.#deafult_fill),
-                        radius: 4,
-                        stroke: new Stroke(me.#default_stroke),
-                    });
-                } catch (e) {
-                    console.error(e);
-                }
+                icon = new CircleStyle({
+                    fill: new Fill(me.#deafult_fill),
+                    radius: 4,
+                    stroke: new Stroke(me.#default_stroke),
+                });
             }
             if (icon) style.setImage(icon);
 
@@ -160,13 +174,11 @@ export class StyleFactory extends MOFactory {
      * @param {*} feature
      * @returns {Text}
      */
-    #getTextStyle(feature, resolution, placement = "point") {
+    #getTextStyle(feature, resolution) {
         let textOption = structuredClone(this.#default_text);
-        textOption.placement = placement;
         textOption["stroke"] = new Stroke(this.#default_text_stroke);
         textOption["fill"] = new Fill(this.#default_text_fill);
-        textOption["text"] =
-            feature.get(this.layerCode[KEY.LABEL_COLUMN]) ?? "";
+        textOption["text"] = feature.get(this.layerCode[KEY.LABEL_COLUMN]) ?? "";
         return new Text(textOption);
     }
 
@@ -182,7 +194,7 @@ export class StyleFactory extends MOFactory {
 
             //2. layerCode에 텍스트 컬럼 지정되었으면
             if (me.layerCode[KEY.LABEL_COLUMN]) {
-                style.setText(me.#getTextStyle(feature, resolution, "line"));
+                style.setText(me.#getTextStyle(feature, resolution));
             }
             return style;
         }
@@ -190,24 +202,46 @@ export class StyleFactory extends MOFactory {
     }
     #getStyleFunc_POLYGON() {
         let me = this;
+        /**
+         * 
+         * @param {Feature} feature 
+         * @param {Number} resolution 
+         * @returns 
+         */
         function styleFunc(feature, resolution) {
             let style = new Style();
             style.setStroke(new Stroke(me.#default_stroke));
             style.setFill(new Fill(me.#deafult_fill));
+
             //2. layerCode에 텍스트 컬럼 지정되었으면
             if (me.layerCode[KEY.LABEL_COLUMN]) {
-                style.setText(me.#getTextStyle(feature, resolution, "line"));
+                style.setText(me.#getTextStyle(feature, resolution));
             }
-
-            //3. 아이콘이 있을 때
+            
+            //3. 아이콘이 있을 때 폴리곤 스타일과 중심좌표 아이콘 둘 다 반환한다
             if (me.layerCode[KEY.ICON_NAME]) {
-                let iconOption =  Object.assign({}, me.#default_icon, {
-                    src: iconPath + me.layerCode[KEY.ICON_NAME],
-                });
-                let icon = new Icon(iconOption);
-                if(icon) style.setImage(icon);
-            }
-            return style;
+                let styleArr = [style];
+                
+                let geom = feature.getGeometry();
+                let targetGeom;
+                if(geom instanceof Polygon){
+                    targetGeom = geom.getInteriorPoint();
+                }else if(geom instanceof MultiPolygon){
+                    targetGeom = geom.getInteriorPoints();
+                }else{
+                    console.table(me.layerCode);
+                    throw new Error (`Feature 가 폴리곤 또는 멀티폴리곤 아님`)
+                }
+                let style2 = new Style();
+                let icon = new Icon(me.#default_icon);
+                if(icon) style2.setImage(icon);
+                style2.setGeometry(targetGeom);
+
+                styleArr.push(style2);
+                return styleArr;
+            }else{
+                return style;
+            }            
         }
         return styleFunc;
     }
